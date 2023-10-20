6df91919c7ec589a08a2dbce036999ac1ff7835a
Afif Uddin <afifuddin@Afifs-MacBook-Air.local>
2023-10-20 10:05:29 +0700

    [UPDATE] Instalasi

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,9 +1,7 @@
 import './bootstrap';
 
 import Alpine from 'alpinejs';
-import mask from '@alpinejs/mask'
 
 window.Alpine = Alpine;
 
-Alpine.plugin(mask)
 Alpine.start();
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\ProductController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -24,14 +23,6 @@ Route::get('/dashboard', function () {
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
-
-    Route::get('products', [ProductController::class, 'index'])->name('products.index');
-    Route::get('products/create', [ProductController::class, 'create'])->name('products.create');
-    Route::post('products', [ProductController::class, 'store'])->name('products.store');
-    Route::get('products/{product}/edit', [ProductController::class, 'edit'])->name('products.edit');
-    Route::put('products/{product}', [ProductController::class, 'update'])->name('products.update');
-    Route::delete('product/{product}', [ProductController::class, 'destroy'])->name('products.destroy');
-
 
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
