f1b906cb1ee927e327b36274938c9a1262b358ef
Afif Uddin <afifuddin@Afifs-MacBook-Air.local>
2023-10-20 10:36:25 +0700

    [UPDATE] List Products

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -1,17 +1,28 @@
 <x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
+    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 px-2">
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    Ini Products
-                </div>
-            </div>
+        <div class="flex mt-6 items-center justify-between">
+            <h2 class="font-semibold text-xl">List Products</h2>
+            <button class="bg-gray-100 px-10 py-2 rounded-md font-semibold">Tambah</button>
         </div>
+
+        <div class="grid md:grid-cols-3 grid-cols-1 mt-4 gap-6">
+            @foreach ($products as $product)
+                <div>
+                    <img src="{{ url('storage/' . $product->foto) }}" />
+                    <div class="my-2">
+                        <p class="text-xl font-light">{{ $product->nama }}</p>
+                        <p class="font-semibold text-gray-400">Rp. {{ number_format($product->harga) }}</p>
+                    </div>
+                    <button class="bg-gray-100 px-10 py-2 w-full rounded-md font-semibold">Edit</button>
+                </div>
+            @endforeach
+        </div>
+
+
+        <div class="mt-4">
+            {{ $products->links() }}
+        </div>
+
     </div>
 </x-app-layout>
